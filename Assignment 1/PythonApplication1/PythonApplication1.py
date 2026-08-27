d228b4ff0b779511e7d3f87751c3f3af5c4036ce
cvharper <145227965+cvharper@users.noreply.github.com>
2026-08-27 16:50:44 -0500

    Add files via upload

---
+++ Assignment 1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,72 @@
+# problem 1
+print("Enter your first name:")
+first = str(input())
+
+print("Enter your last name:")
+last = str(input())
+
+print(f"Welcome {first} {last}")
+
+print("Hit Enter to move on to next simulation.")
+proceed = input()
+
+# problem 2
+
+# 10 times
+i = 1
+while i <= 10:
+  print(f"Ran program {i}/10 times")
+  i += 1
+
+print("Hit Enter to move on to next simulation.")
+proceed = input()
+
+# 1000 times
+i = 1
+while i <= 1000:
+  print(f"Ran program {i}/1000 times")
+  i += 1
+
+print("Hit Enter to move on to next simulation.")
+proceed = input()
+
+# 10000 times
+i = 1
+while i <= 10000:
+  print(f"Ran program {i}/10000 times")
+  i += 1
+
+print("Hit Enter to move on to next simulation.")
+proceed = input()
+
+# problem 3
+
+print(f"The value of True is: {int(True)}")
+print(f"The value of False is: {int(False)}")
+
+print("Hit Enter to move on to next simulation.")
+proceed = input()
+
+# problem 4
+
+print("Enter a value for A")
+a = float(input())
+
+print("Enter a value for B")
+b = float(input())
+
+result = a * b
+
+print(f"Product of {a} and {b} is {result}")
+
+print("Hit Enter to move on to next simulation.")
+proceed = input()
+
+
+# problem 5
+
+int = 3
+
+print(f"This is a float: {result}")
+print(f"This is a str: {first}")
+print(f"This is an int: {int}")
